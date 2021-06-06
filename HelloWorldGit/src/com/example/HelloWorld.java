ca59ae51cd70eac28c1bcae8fa2c48d5751100bf
Swapnil Warekar <swapnilwarekar@gmail.com>
2021-06-06 12:03:52 +0530

    Initial Commit

---
+++ HelloWorldGit/src/com/example/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.example;
+
+/**
+ * @author swapnilwarekar
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World Git");
+
+	}
+
+}
